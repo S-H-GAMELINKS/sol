e0724540011706c4a344e5d44a17f740fbe974af
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-16 22:23:49 +0900

    using namespace for fs #10

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <experimental/filesystem>
 
+namespace fs = std::experimental::filesystem;
+
 int main(int argc, char **argv) {
     CLI::App app;
 
@@ -14,8 +16,8 @@ int main(int argc, char **argv) {
 
     if (new_app) {
         std::cout << "Created New App " << app_name << std::endl;
-        std::experimental::filesystem::copy(std::experimental::filesystem::absolute("sol/src"), app_name);
-        std::experimental::filesystem::copy(std::experimental::filesystem::absolute("sol/cpp-httplib"), app_name + "/cpp-httplib");
+        fs::copy(fs::absolute("sol/src"), app_name);
+        fs::copy(fs::absolute("sol/cpp-httplib"), app_name + "/cpp-httplib");
     }
 
     return 0;
