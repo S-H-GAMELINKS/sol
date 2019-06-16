4aa2b076afab6026fc4dcb83ac43a4a26ba461c0
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-16 16:44:27 +0900

    add new sub command #5

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <CLI/CLI.hpp>
+#include <fstream> 
 
 int main(int argc, char **argv) {
     CLI::App app;
 
-    // Add new options/flags here
+    // Define options
+    CLI::App *new_app = app.add_subcommand("new", "Create New App")->required();
+    std::string name;
+    new_app->add_option("app", name, "App name");
 
     CLI11_PARSE(app, argc, argv);
+
+    if (new_app) {
+        std::cout << "New App Name is " << name << std::endl;
+    }
 
     return 0;
 }
