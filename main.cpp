c671339391d79e5fd7c59e31a4ecc83af3d476e3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-16 16:50:18 +0900

    using filesystem for new sub command #5

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <CLI/CLI.hpp>
-#include <fstream> 
+#include <fstream>
+#include <experimental/filesystem>
 
 int main(int argc, char **argv) {
     CLI::App app;
 
     // Define options
     CLI::App *new_app = app.add_subcommand("new", "Create New App")->required();
-    std::string name;
-    new_app->add_option("app", name, "App name");
+    std::string app_name;
+    new_app->add_option("app", app_name, "App name");
 
     CLI11_PARSE(app, argc, argv);
 
     if (new_app) {
-        std::cout << "New App Name is " << name << std::endl;
+        std::cout << "Created New App " << app_name << std::endl;
+        std::experimental::filesystem::create_directory(app_name);
     }
 
     return 0;
