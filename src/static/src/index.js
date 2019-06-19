f1d3a8d63ed5f9f6b0fe1652432b5ac860d675ee
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-19 12:43:01 +0900

    import stimulus controller #4

--- src/static/src/index.js
+++ src/static/src/index.js
@@ -1,3 +1,10 @@
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 import App from './App.svelte';
 
 const app = new App({
