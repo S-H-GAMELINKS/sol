c7d72c2273d24148d949ae1ac363c2b69f81bd98
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-06-19 21:16:23 +0900

    fix require sub command #21

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
     CLI::App app;
 
     // Define options
-    CLI::App *new_app = app.add_subcommand("new", "Create New App")->required();
+    CLI::App *new_app = app.add_subcommand("new", "Create New App");
     std::string app_name;
     new_app->add_option("app", app_name, "App name");
 
@@ -18,7 +18,7 @@ int main(int argc, char **argv) {
     fs::path p = "/proc/self/exe";
     std::string path = std::regex_replace(fs::read_symlink(p).string<char>(), std::regex("a.out"), "");
 
-    if (new_app) {
+    if (app.got_subcommand(new_app)) {
         std::cout << "Created New App " << app_name << std::endl;
         fs::copy(path + "/src", app_name, fs::copy_options::recursive);
         fs::copy(path + "/cpp-httplib", app_name + "/cpp-httplib");
