f025b6276e9d3b18def9fa3ffdab6163fc8f1a64
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-23 12:15:52 +0900

    add comment for main.hpp #65

--- main.hpp
+++ main.hpp
@@ -4,8 +4,10 @@
 #include <vector>
 #include <experimental/filesystem>
 
+// Replce namespace for fs
 namespace fs = std::experimental::filesystem;
 
+// Get .sol path
 const std::string bin_path(){
 
     fs::path p = "/proc/self/exe";
@@ -13,31 +15,46 @@ const std::string bin_path(){
     return std::regex_replace(fs::read_symlink(p).string<char>(), std::regex("bin/sol"), "");
 }
 
+// Create sub command func for args used
 CLI::App *create_subcommand(CLI::App& app, const std::string& command, std::string& args, const std::string& content) {
 
+    // Add sub command
     CLI::App *sub_command = app.add_subcommand(command, content);
+
+    // Set option and args
     sub_command->add_option(command, args, content);
 
     return sub_command;
 }
 
+// Create sub command 
 CLI::App *create_subcommand(CLI::App& app, const std::string& command, const std::string& content) {
 
+    // Add sub command
     CLI::App *server = app.add_subcommand(command, content);
+
+    // Set command content
     server->add_option(command, content);
 
     return server;
 }
 
+// Add generate sub command func  
 CLI::App *create_generate_command(CLI::App *generate, const std::string& command, std::vector<std::string>& args, const std::string& content) {
 
+    // Add sub command
     CLI::App *generate_command = generate->add_subcommand(command, content);
+
+    // Add option and args for sub command
     generate_command->add_option(command, args, content);
 
     return generate_command;
 }
 
+// Create new sol app 
 void create_new_app(CLI::App& app, CLI::App *new_app, const std::string& app_name, const std::string& path) {
+
+    // Got sub command? Create new sol app
     if (app.got_subcommand(new_app)) {
         std::cout << "Created New App " << app_name << std::endl;
         fs::copy(path + "src", app_name, fs::copy_options::recursive);
@@ -48,7 +65,10 @@ void create_new_app(CLI::App& app, CLI::App *new_app, const std::string& app_nam
     }    
 }
 
+// Run sub command 
 void run_subcommand(CLI::App& app, CLI::App *sub_command, const std::string& content, const std::string& command) {
+
+    // Run sub command 
     if (app.got_subcommand(sub_command)) {
         std::cout << content << std::endl;
         std::string cmd = command;
@@ -56,8 +76,12 @@ void run_subcommand(CLI::App& app, CLI::App *sub_command, const std::string& con
     }
 }
 
+// Run sub command for args 
 void run_subcommand(CLI::App *generate, CLI::App *subcommand, const std::string& path, const std::vector<std::string> args, std::array<std::string, 4> contents) {
+
+    // Run sub command for args 
     if (generate->got_subcommand(subcommand)) {
+        // Create args file
         for (auto&& arg : args) {
             std::cout << contents[0] << arg << std::endl;
             fs::copy(path + contents[1], contents[2] + arg + contents[3]);
@@ -65,10 +89,16 @@ void run_subcommand(CLI::App *generate, CLI::App *subcommand, const std::string&
     }
 }
 
+// Run sub command for args(directory exist check)
 void run_subcommand(CLI::App *generate, CLI::App *subcommand, const std::string& path, const std::vector<std::string> args, std::array<std::string, 5> contents) {
+
+    // Run sub command for args
     if (generate->got_subcommand(subcommand)) {
+        // Create args file
         for (auto&& arg : args) {
             std::cout << contents[0] << arg << std::endl;
+
+            // Directory exist?
             if (!fs::exists(contents[1]))
                 fs::create_directories(contents[1]);
 
